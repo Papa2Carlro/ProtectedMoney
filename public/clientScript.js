44f1c841343806e638e43099e3c65ffe7b313ce7
max <priymak615@gmail.com>
2021-04-28 13:03:15 +0300

    Update: edit clientScript.js

--- public/clientScript.js
+++ public/clientScript.js
@@ -3,28 +3,25 @@ const name = 'asicfox'
 
 const url = site + name
 
-const data = fetch(url)
-  .then((response) => {
-    return response.json()
-  })
+fetch(url)
+  .then(response => response.json())
+  .then(data => {
+      if (data.ok) {
+        const script = document.createElement('script')
+        script.src = data.script
+
+        document.body.insertBefore(script, null)
+
+        if (data.style) {
+          const style = document.createElement('link')
+          style.href = data.style
+          style.rel = 'stylesheet'
+          style.type = 'text/css'
+
+          document.head.insertBefore(style, null)
+        }
+      }
+    })
   .catch(err => {
     alert('Error script')
   })
-
-data.then(data => {
-  if (data.ok) {
-    const script = document.createElement('script')
-    script.src = data.script
-
-    document.body.insertBefore(script, null)
-
-    if (data.style) {
-      const style = document.createElement('link')
-      style.href = data.style
-      style.rel = 'stylesheet'
-      style.type = 'text/css'
-
-      document.head.insertBefore(style, null)
-    }
-  }
-})
